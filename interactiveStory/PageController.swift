b2c1d4dceedc86c97c5000bdcdaf028986d62eaf
Kevin VanEvery <kvanever@gmail.com>
2016-07-10 14:18:00 -0700

    add page initialization methods

---
+++ interactiveStory/PageController.swift
@@ -0,0 +1,47 @@
+//
+//  PageController.swift
+//  interactiveStory
+//
+//  Created by Kevin VanEvery on 7/10/16.
+//  Copyright © 2016 Kevin VanEvery. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIViewController {
+    
+    var page: Page?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(page: Page) {
+        self.page = page
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blueColor()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
