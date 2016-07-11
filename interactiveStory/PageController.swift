8d05cdc9f74b12bc428be539972985e655f13823
Kevin VanEvery <kvanever@gmail.com>
2016-07-10 18:07:17 -0700

    add sound effect function to button click

--- interactiveStory/PageController.swift
+++ interactiveStory/PageController.swift
@@ -110,6 +110,8 @@ class PageController: UIViewController {
             let nextPage = firstChoice.page;
             let pageController = PageController(page: nextPage)
             
+            playSound(nextPage.story.soundEffectURL)
+            
             navigationController?.pushViewController(pageController, animated: true)
         }
     }
@@ -119,12 +121,19 @@ class PageController: UIViewController {
             let nextPage = secondChoice.page;
             let pageController = PageController(page: nextPage)
             
+            playSound(nextPage.story.soundEffectURL)
+            
             navigationController?.pushViewController(pageController, animated: true)
         }
     }
     
     func playAgain() {
         navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    func playSound(url: NSURL) {
+        AudioServicesCreateSystemSoundID(url, &sound)
+        AudioServicesPlaySystemSound(sound)
     }
     
 }
